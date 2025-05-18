178513b46a4592b93463eeede8bf0683f6c40e28
AkakiGurgenidze <agurgenidze02@gmail.com>
2025-05-18 22:18:37 +0400

    Dont require pem files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import * as fs from 'node:fs';
 
 export default defineConfig(() => ({
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
     port: 3000,
   },
   plugins: [react()],
